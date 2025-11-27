718737bf0f9422d6484d5008cdfa2715932712ce
ELNASORY Karam <elnasorykaram@gmail.com>
2025-11-27 16:23:09 +0100

    Refactor Main class to implement MCB problem search functionality

--- TP1 & TP2/Ex4/Main.java
+++ TP1 & TP2/Ex4/Main.java
@@ -1,9 +1,9 @@
-package Ex4;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, this is Exercise 4!");
+
+public class Main {
+
+    public static void main(String[] args) {
+        MCB_Problem mcb = new MCB_Problem();
+        AlgoRecherche.recherche(mcb);
     }
 }
