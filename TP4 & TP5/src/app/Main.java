06d0177fa2e4f1a2947653f8f3addef9f50dde18
ErkinTunc <erkintuncboya@hotmail.com>
2025-12-12 17:22:36 +0100

    corrected file us_capitals route

--- TP4 & TP5/src/app/Main.java
+++ TP4 & TP5/src/app/Main.java
@@ -24,11 +24,11 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("src/us_capitals.txt");
+        File file = new File("us_capitals.txt");
         ArrayList<City> cities = City.readCitiesFromFile(file);
         System.out.println("Number of cities loaded: " + cities.size());
 
-        City startCity = cities.get(10); // Assuming the first city is the starting point
+        City startCity = cities.get(0); // Assuming the first city is the starting point
         Problem problem = new Problem(cities, startCity);
 
         Node solutionNode = ResearchAlgo.depthFirstSearch(problem);
